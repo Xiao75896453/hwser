105fcbc1fe98332e3be70fd0ad83279e590b0210
Your Name <a75896453@gmail.com>
2019-04-07 13:19:05 +0800

    hwser

--- dist/hw.js
+++ dist/hw.js
@@ -1,22 +1,93 @@
-$(document).ready(function() {
-    $('#listallbtn').on('click',function(){
-      listall();
+$(document).ready(function () {
+    $('#listallbtn').on('click', function () {
+        event.preventDefault()
+        listall();
+    });
+    $('#searchbtn').on('click', function () {
+        event.preventDefault()
+        const ID = $("#ajax-search input[name=sstudentID]").val();
+        search(ID);
+    });
+    $('#addbtn').on('click', function () {
+        event.preventDefault()
+        const addID = $("#ajax-add input[name=astudentID]").val();
+        const addstudnet = $("#ajax-add input[name=astudentname]").val();
+        add(addID, addstudnet);
+    });
+    $('#deletebtn').on('click', function () {
+        event.preventDefault()
+        const delID = $("#ajax-delete input[name=dstudentID]").val();
+        del(delID);
     });
 
-function listall(){
-  $.ajax({
-    url:"../students.sample.json",
-    type:"post",
-    datatype:"json",
-    success: function(response){
-    $.each(response, function(key,value) {
-        $('#ajax-listall').append(
-            &('<div>',{text:[key]+","+[valut]};
-        )
-        });
-    },
-    error:function(){
-      alert("ERROR");}
-  });
+})
+function listall() {
+    $.ajax({
+        url: "listall",
+        type: "POST",
+        datatype: "json",
+        success: function (response) {
+            //console.log(response);
+            $("#ajax-listall").html(response);
+        },
+        error: function () {
+            alert("ERROR");
+        }
+    });
+};
+function search(ID) {
+    const inputdata = {};
+    inputdata['ID'] = ID;
 
- 
+    $.ajax({
+        url: "search",
+        type: "POST",
+        data : inputdata,
+        datatype: "json",
+        success: function (response) {
+            //console.log(response);
+            $("#ajax-target").html(response);
+        },
+        error: function () {
+            alert("ERROR");
+        }
+    });
+};
+
+function add(addID, addstudnet) {
+    const inputdata = {};
+    inputdata['ID'] = addID;
+    inputdata['name'] = addstudnet;
+    $.ajax({
+        url: "add",
+        type: "POST",
+        data: inputdata,
+        datatype: "json",
+        success: function (response) {
+            listall()
+            //console.log(response);
+        },
+        error: function () {
+            alert("ERROR");
+        }
+    });
+}
+
+function del(delID) {
+    const inputdata = {};
+    inputdata['ID'] = delID;
+
+    $.ajax({
+        url: "del",
+        type: "POST",
+        data: inputdata,
+        datatype: "json",
+        success: function (response) {
+            listall()
+            //console.log(response);
+        },
+        error: function () {
+            alert("ERROR");
+        }
+    });
+}
--- hwser.js
+++ hwser.js
@@ -2,30 +2,30 @@
 
 // Step 1 and step 2 code goes here
 //載入 `express`
- const express = require('express')
+const express = require('express')
 // // 建立一個 express (也就是網頁伺服器)實體
- const app = express()
- const port = 11344
+const app = express()
+const port = 11344
 
 // 啟動伺服器
- app.listen(port, () => {
- console.log(`listening on port: ${port}`)
- })
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
 
 // Step 3 code goes here
- app.use(express.static(`${__dirname}/dist`))
+app.use(express.static(`${__dirname}/dist`))
 
 // 載入 `body-parser`
- const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 // 設定 `body-parser`
- app.use(bodyParser.urlencoded({ extended: false }))
- app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
- app.post('/step7', (req, res) => {
-// `bady-parser` 將解析好的資料存放在 `req.body`
- res.send(`<h1>Hello, ${req.body.fname} ${req.body.lname}</h1>`)
- })
+app.post('/step7', (req, res) => {
+    // `bady-parser` 將解析好的資料存放在 `req.body`
+    res.send(`<h1>Hello, ${req.body.fname} ${req.body.lname}</h1>`)
+})
 
 'use strict';
 
@@ -34,3 +34,40 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('students.sample.json');  
 let student = JSON.parse(rawdata);  
 console.log(student); 
+
+app.post('/listall', (req, res) => {
+    //res.send(student)
+    var listalltohtml="";
+    for (key in student){
+        const element = `<p>"${key}":"${student[key]}"</p>`;
+        listalltohtml += element;
+    }
+    res.send(listalltohtml)
+});
+
+app.post('/search', (req, res) => {
+    //res.send(student)
+    var key = req.body['ID'];
+    const search = `<p>"${key}":"${student[key]}"</p>`;
+    res.send(search)
+});
+
+app.post('/add', (req, res) => {
+    //res.send(student)
+    var key = req.body['ID'];
+    var name = req.body['name'];
+    student[key]=name;
+    res.send(name)
+    let data = JSON.stringify(student);  
+    fs.writeFileSync('students.sample.json', data);
+});
+app.post('/del', (req, res) => {
+    //res.send(student)
+    //var key = req.body['ID'];
+    //console.log("2");
+    //console.log(key);
+    //console.log(student[key]);
+    delete student[req.body['ID']];
+    let data = JSON.stringify(student);  
+    fs.writeFileSync('students.sample.json', data);
+});
